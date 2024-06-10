d3ac9cd89abd0f65b75a44cb53cedde112d58881
Sammm <ntdsam@gmail.com>
2024-06-10 10:19:17 +0700

    first commit

---
+++ vite-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,7 @@
+import Layout from "../../components/layout/Layout";
+
+const HomePage = () => {
+  return <Layout>Home Page</Layout>;
+};
+
+export default HomePage;
---
+++ vite-project/src/pages/noPage/NoPage.jsx
@@ -0,0 +1,7 @@
+import Layout from "../../components/layout/Layout";
+
+const HomePage = () => {
+  return <Layout>No Page</Layout>;
+};
+
+export default HomePage;
