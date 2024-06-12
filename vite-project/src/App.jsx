e59d7257bd7019fd124d45277c538277d5211be6
Sammm <ntdsam@gmail.com>
2024-06-12 22:44:26 +0700

    Book in4

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import BookInfo from "./pages/bookInfo/BookInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
