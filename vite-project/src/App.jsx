5fad17c3ece74506bf24533a81123aae7f207f71
Sammm <ntdsam@gmail.com>
2024-06-12 23:00:28 +0700

    All Books

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import BookInfo from "./pages/bookInfo/BookInfo";
-import ScrollTop from "./components/scrollTop/ScrollTop";
+import AllBooks from "./pages/allBooks/AllBooks";
 
+import ScrollTop from "./components/scrollTop/ScrollTop";
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
           <Route path="/bookinfo" element={<BookInfo />} />
+          <Route path="/allbooks" element={<AllBooks />} />
         </Routes>
       </Router>
     </div>
--- vite-project/src/pages/allBooks/AllBooks.jsx
+++ vite-project/src/pages/allBooks/AllBooks.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
+import Layout from "../../components/layout/Layout";
 
 // productData
 const bookData = [
@@ -82,59 +83,63 @@ const bookData = [
   },
 ];
 
-const HomePageBookCard = () => {
+const AllBooks = () => {
   const navigate = useNavigate();
   return (
-    <div className="mt-10">
-      {/* Heading  */}
-      <div className="">
-        <h1 className=" text-center mb-5 text-2xl font-semibold">New Books</h1>
-      </div>
+    <Layout>
+      <div className="py-8">
+        {/* Heading  */}
+        <div className="">
+          <h1 className=" text-center mb-5 text-2xl font-semibold">
+            All Books
+          </h1>
+        </div>
 
-      {/* main  */}
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-5 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            {bookData.map((item, index) => {
-              const { image, name, authors } = item;
-              return (
-                <div key={index} className="p-3.5 w-full md:w-1/4">
-                  <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
-                    <img
-                      onClick={() => navigate("/bookinfo")}
-                      className="lg:h-80  h-96 w-full"
-                      src={image}
-                      alt="blog"
-                    />
-                    <div className="p-6">
-                      <h2 className="tracking-widest text-xs name-font font-medium text-gray-400 mb-1">
-                        My Book Catalog
-                      </h2>
-                      <h1 className="name-font text-lg font-semibold text-gray-900 mb-3">
-                        {name.substring(0, 25)}
-                      </h1>
-                      <h1 className="name-font text-lg font-medium text-gray-900 mb-3">
-                        {authors}
-                      </h1>
+        {/* main  */}
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 lg:px-0 py-5 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              {bookData.map((item, index) => {
+                const { image, name, authors } = item;
+                return (
+                  <div key={index} className="p-3.5 w-full md:w-1/4">
+                    <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
+                      <img
+                        onClick={() => navigate("/bookinfo")}
+                        className="lg:h-80  h-96 w-full"
+                        src={image}
+                        alt="blog"
+                      />
+                      <div className="p-6">
+                        <h2 className="tracking-widest text-xs name-font font-medium text-gray-400 mb-1">
+                          My Book Catalog
+                        </h2>
+                        <h1 className="name-font text-lg font-semibold text-gray-900 mb-3">
+                          {name.substring(0, 25)}
+                        </h1>
+                        <h1 className="name-font text-lg font-medium text-gray-900 mb-3">
+                          {authors}
+                        </h1>
 
-                      <div className="flex justify-center ">
-                        <button
-                          onClick={() => navigate("/bookinfo")}
-                          className=" bg-indigo-200 hover:bg-indigo-600 w-full text-white py-[4px] rounded-lg font-bold"
-                        >
-                          More details
-                        </button>
+                        <div className="flex justify-center ">
+                          <button
+                            onClick={() => navigate("/bookinfo")}
+                            className=" bg-indigo-200 hover:bg-indigo-600 w-full text-white py-[4px] rounded-lg font-bold"
+                          >
+                            More details
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </Layout>
   );
 };
 
-export default HomePageBookCard;
+export default AllBooks;
