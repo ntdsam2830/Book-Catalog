9aab844dcc0871c2522523c8167bc2c9d07508dc
Sammm <ntdsam@gmail.com>
2024-06-15 18:02:41 +0700

    fix BookDetail

--- vite-project/src/components/user/BookDetail.jsx
+++ vite-project/src/components/user/BookDetail.jsx
@@ -4,17 +4,25 @@ import myContext from "../../context/myContext";
 import Loader from "../loader/Loader";
 
 const BookDetail = () => {
+  const context = useContext(myContext);
+  const { loading, getAllBook } = context;
+
   return (
     <div>
       <div className="py-5 flex justify-between items-center">
         {/* text  */}
-        <h1 className=" text-xl text-indigo-300 font-bold">All Product</h1>
+        <h1 className=" text-xl text-indigo-300 font-bold">All Books</h1>
         {/* Add Product Button  */}
         <Link to={"/addbook"}>
           <button className="px-5 py-2 bg-indigo-50 border border-indigo-100 rounded-lg">
             Add Book
           </button>
         </Link>
+      </div>
+
+      {/* Loading  */}
+      <div className="flex justify-center relative top-20">
+        {loading && <Loader />}
       </div>
 
       {/* table  */}
@@ -71,32 +79,40 @@ const BookDetail = () => {
                 Action
               </th>
             </tr>
-            <tr className="text-indigo-300">
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 ">
-                1.
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                <img src="" alt="" />
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"name"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"auhtors"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"category"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"publish"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
-                Edit
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-indigo-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
-                Delete
-              </td>
-            </tr>
+
+            {getAllBook.map((item, index) => {
+              const { name, authors, category, publish, bookImageUrl } = item;
+              return (
+                <tr key={index} className="text-pink-300">
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 ">
+                    {index + 1}.
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    <div className="flex justify-center">
+                      <img className="w-20 " src={bookImageUrl} alt="" />
+                    </div>
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {name}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {authors}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {category}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {publish}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
+                    Edit
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
+                    Delete
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
