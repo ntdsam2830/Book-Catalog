1feaeb6df54e499bb1326724e8ee165ffe6e4bc7
Sammm <ntdsam@gmail.com>
2024-06-18 14:16:03 +0700

    search logic

--- vite-project/src/components/searchBar/SearchBar.jsx
+++ vite-project/src/components/searchBar/SearchBar.jsx
@@ -1,50 +1,16 @@
-import { useState } from "react";
-
-// Search Data
-const searchData = [
-  {
-    name: "Fashion",
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-  },
-  {
-    name: "Shirt",
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-  },
-  {
-    name: "Jacket",
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-  },
-  {
-    name: "Mobile",
-    image:
-      "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-  },
-  {
-    name: "Laptop",
-    image:
-      "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-  },
-  {
-    name: "Home",
-    image:
-      "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-  },
-  {
-    name: "Book",
-    image:
-      "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-  },
-];
+import { useContext, useState } from "react";
+import myContext from "../../context/myContext";
+import { useNavigate } from "react-router";
 
 const SearchBar = () => {
+  const context = useContext(myContext);
+  const { getAllBook } = context;
   // Search State
   const [search, setSearch] = useState("");
 
+  const navigate = useNavigate();
   // Filter Search Data
-  const filterSearchData = searchData
+  const filterSearchData = getAllBook
     .filter((obj) => obj.name.toLowerCase().includes(search))
     .slice(0, 8);
   return (
@@ -67,9 +33,13 @@ const SearchBar = () => {
               <>
                 {filterSearchData.map((item, index) => {
                   return (
-                    <div key={index} className="py-2 px-2">
+                    <div
+                      key={index}
+                      className="py-2 px-2 cursor-pointer"
+                      onClick={() => navigate(`/bookinfo/${item.id}`)}
+                    >
                       <div className="flex items-center gap-2">
-                        <img className="w-10" src={item.image} alt="" />
+                        <img className="w-10" src={item.bookImageUrl} alt="" />
                         {item.name}
                       </div>
                     </div>
